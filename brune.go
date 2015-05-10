552d272c28b9885c3673108b31ff5188bfe3807e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2015-05-10 01:24:11 -0700

    Fix the centering.

--- brune.go
+++ brune.go
@@ -12,7 +12,7 @@ var file []byte
 var jsontype jsonobject
 
 func bruneHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<html><link href='http://fonts.googleapis.com/css?family=Reenie+Beanie'rel='stylesheet' type='text/css'><style>span { font-family: 'Reenie Beanie', cursive; } div {font-size: 62; position: fixed; top: 50%%;  left: 50%%;  /* bring your own prefixes */  transform: translate(-50%%, -50%%);}</style><div><span>Brune blamed:&nbsp<em>%v</em></div></html>", jsontype.Object.Counter)
+    fmt.Fprintf(w, "<html><link href='http://fonts.googleapis.com/css?family=Reenie+Beanie'rel='stylesheet' type='text/css'><style>body {position: relative;} span { font-family: 'Reenie Beanie', cursive; } div { font-size: 10em; position: absolute; top: 50%%; left: 50%%; transform: translate(-50%%, -50%%);}</style><div><span>Blame Brune:&nbsp<em>%v</em></div></html>", jsontype.Object.Counter)
 	data, err := json.Marshal(jsontype)
 	jsontype.Object.Counter++
 	err = ioutil.WriteFile("config.json", data, 755)
