23a48d9a02d6eae443853b2aeef334946367a603
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2015-05-10 00:31:37 -0700

    Blame all the brunes..

--- brune.go
+++ brune.go
@@ -11,16 +11,10 @@ import (
 var file []byte
 var jsontype jsonobject
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<html><span>\"Brune Blamed\"<span><em>%v</em></html>", jsontype.Object.Counter)
-    jsontype.Object.Counter++
-}
-
 func bruneHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<html><em>%v</em><form action=\"/Blame Brune\" method=\"POST\">"+
-        "<input type=\"submit\" value=\"Blame Brune\">"+
-        "</form></html>", jsontype.Object.Counter)
+    fmt.Fprintf(w, "<html><link href='http://fonts.googleapis.com/css?family=Reenie+Beanie'rel='stylesheet' type='text/css'><style>span { font-family: 'Reenie Beanie', cursive; } div {font-size: 62; position: fixed; top: 50%%;  left: 50%%;  /* bring your own prefixes */  transform: translate(-50%%, -50%%);}</style><div><span>Brune blamed:&nbsp<em>%v</em></div></html>", jsontype.Object.Counter)
 	data, err := json.Marshal(jsontype)
+	jsontype.Object.Counter++
 	err = ioutil.WriteFile("config.json", data, 755)
 	if err != nil {
 		fmt.Println("Can't write file.")
@@ -43,8 +37,7 @@ func main() {
         fmt.Printf("File error: %v\n", e)
     }
     json.Unmarshal(file, &jsontype)
-    http.HandleFunc("/", handler)
-	http.HandleFunc("/Blame Brune", bruneHandler)
+	http.HandleFunc("/", bruneHandler)
     http.ListenAndServe(":8080", nil)
 	fmt.Print(jsontype)
 }
